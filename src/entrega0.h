b4610e1ce853e9ea8ecc778b22c1d4a2e7a7bb34
Christian-hub-art <christianbecerra@javeriana.edu.co>
2025-08-08 22:17:52 -0500

    Update entrega0.h

--- src/entrega0.h
+++ src/entrega0.h
@@ -3,6 +3,6 @@
 #include <vector>
 using namespace std;
 
-bool esComandoValido(const string& comando);
-void mostrarComandos();
-void ayudaComando(const string& comando);
+bool esComandoValido(const string& comando); // de define la funcion de comando valido
+void mostrarComandos(); // se define la funcion de mostrar comandos
+void ayudaComando(const string& comando); // se define la funcion de ayuda que mostrara los detalles de cada comando
