7bc8d1199f36d372a598f9f12658a2065b2266e0
Christian-hub-art <christianbecerra@javeriana.edu.co>
2025-08-08 22:03:41 -0500

    Update entrega0.cxx

--- src/entrega0.cxx
+++ src/entrega0.cxx
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 #include "entrega0.h"
 using namespace std; 
 
@@ -12,7 +13,13 @@ bool esComandoValido(const string& comando) {
         "es_subsecuencia",
         "enmascarar",
         "guardar", 
-        "ayuda"
+        "ayuda",
+        "ayuda cargar nombre_archivo",
+        "ayuda listar_secuencias",
+        "ayuda histograma descripcion_secuencia",   
+        "ayuda es_subsecuencia",
+        "ayuda enmascarar",
+        "ayuda guardar", 
 };
 
     for (const string& valido : comandosValidos) {
@@ -21,4 +28,48 @@ bool esComandoValido(const string& comando) {
         }
     }
     return false;
+}
+
+void mostrarComandos(){
+    cout<<"Comandos validos:"<<endl;
+    cout<<"cargar nombre_archivo"<<endl;
+    cout<<"listar_secuencias"<<endl;
+    cout<<"histograma descripcion_secuencia"<<endl;
+    cout<<"es_subsecuencia"<<endl;
+    cout<<"enmascarar"<<endl;
+    cout<<"guardar"<<endl;
+    cout<<"salir"<<endl;
+    cout<<"ayuda cargar nombre_archivo"<<endl;
+    cout<<"ayuda listar_secuencias"<<endl;
+    cout<<"ayuda histograma descripcion_secuencia"<<endl;   
+    cout<<"ayuda es_subsecuencia"<<endl;
+    cout<<"ayuda enmascarar"<<endl;
+    cout<<"ayuda guardar"<<endl;
+}
+
+void ayudaComando(const string& comando){
+    vector<string> comandos = {
+    "ayuda cargar nombre_archivo",
+    "ayuda listar_secuencias",
+    "ayuda histograma descripcion_secuencia",   
+    "ayuda es_subsecuencia",
+    "ayuda enmascarar",
+    "ayuda guardar", 
+    };
+    vector<string> descripcion = {
+    "Carga en memoria los datos contenidos en el             archivo identificado por nombre_archivo",
+    "Imprime la cantidad de secuencias actualmente en        memoria, y luego, en n líneas la información            básica de cada secuencia.", 
+    "Imprime el histograma de una secuencia, en caso de      que exista",
+    "Determina si una subsecuencia dada por el usuario,      existe dentro de las secuencias cargadas en             memoria.",
+    "Enmascara una secuencia dada por el usuario, en         caso de que exista.",
+    "Guarda en el archivo nombre_archivo las secuencias      cargadas en memoria"
+    };
+    
+    for (int i =0; i<comandos.size(); i++){
+        if (comando == comandos[i]){
+            cout<<descripcion[i]<<endl;
+            break;
+        }
+    }
+
 }
