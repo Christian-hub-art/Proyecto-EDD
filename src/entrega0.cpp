9afb7a861f11c4779fd49cf309d5bb77401aa020
Christian-hub-art <christianbecerra@javeriana.edu.co>
2025-08-08 22:30:14 -0500

    Update entrega0.cpp

--- src/entrega0.cpp
+++ src/entrega0.cpp
@@ -6,25 +6,25 @@ using namespace std;
 
 
 int main(){
-    string comando;
-    while(true){
+    string comando; // se define una variable tipo string para alamecenar lo que escribe el usuario
+    while(true){ // el while true nos sirve para generar ciclos infinitos hasta que el usuario decida colocar el comando salir
         cout<<"$";
-        getline(cin,comando);
+        getline(cin,comando); // almacena con espacios la cadena que digitalice el usuario
        
-        if (comando=="salir"){
+        if (comando=="salir"){ // comando salir rompe el ciclo y termina con el prgrama 
             break;
         }
-        if (comando=="ayuda"){
+        if (comando=="ayuda"){ // con elcoamndo ayuda se muestran todos los comandos disponibles que hay
             mostrarComandos();
         }
         
-        ayudaComando(comando);
+        ayudaComando(comando); // si digitaliza algun comando ayuda comando mostrara los detalles de aquel comando 
         
-        if(esComandoValido(comando)){
+        if(esComandoValido(comando)){  // le dice al usuario si el comando que dgitalizo si se encuentra o no
             cout<<"Comando ingresado Correctamente "<<endl;
         }
         else{
-            cout<<"Comando invalido. Revise nuevamente la guia con el comando de ayuda"<<endl;
+            cout<<"Comando invalido. Revise nuevamente la guia con el comando de ayuda"<<endl; // valida que el comando se digitalizo mal y le sugiere al usuario el comando ayuda
         }
     }
     
